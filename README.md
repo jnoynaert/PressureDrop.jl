08daaf2dfb45b1cdd74e03c982e7f898d96bd128
Jared Noynaert <jnoynaert@altamesa.net>
2019-03-04 17:38:48 -0600

    Added basic pressure drop outline

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# pressuredrop.jl
+# PressureDrop.jl
 Package for computing pressure profiles for gas lift optimization of oil &amp; gas wells.
+
+# Example usage
+
+TODO
+
+# Supported correlations
+
+TODO
+
+# Performance
+
+Notes on using indirectly via main() etc depending on how you decide to handle argument typing
+
+Add type annotations to your source as needed before completing this section.
---
+++ src/PressureDrop.jl
@@ -0,0 +1,50 @@
+#TODO: Finish profiling using @time, @code_warntype, Traceur, ProfileView
+#TODO: @inbounds master loop
+#TODO: add test suite per Takacs results, or per a reference result from IHS
+
+"""
+Develop pressure traverse from wellhead down to datum.
+
+Inputs: survey, WHP, WHT, BHT,
+GOR,
+gas s.g., water s.g., oil API
+which correlations to use for fluid properties
+which pressure drop correlation to use
+
+Returns: pressure profile, temperature profile as two separate Array{Float64, 1}s.
+
+Automatically meshes the deviation profile without resizing etc -- keep that separate as a utility.
+"""
+function traverse_topdown()
+    # Takacs 113 figure 2-41
+    # Make sure this is from wellhead down
+    # TODO: calculate BHT?
+    # TODO: use GOR to initialize model, since GLR is ambiguous term for gas lift opt, but GOR is understand to be reservoir #s
+    # TODO: make sure to utilize named arguments
+
+    set ϵ or error maximum
+    set h_initial
+    set dh
+    estimate ΔP_est
+
+    iteration 0:
+    p_avg = p_initial + dp_est/2
+
+    t_avg = t(h_initial + dh/2)
+
+    calculate pvt properties at p_avg, t_avg
+
+    calculate dp/dh
+
+    dp_calc = dh*(dp/dh)
+
+    if abs(dp_est - dp_calc) > ϵ:
+        new dp_est = dp_calc
+        reiterate
+    else
+        h_initial_new = h_initial + dh
+        p_initial_new = p_initial + dp_calc
+
+    if h_initial_new >= td_well:
+        proportionally estimate pwf based on dp_calc/dh
+end
